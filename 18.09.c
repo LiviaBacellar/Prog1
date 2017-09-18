154a57aaf83bb33ca8cdbf0ea22e1e04c3a8eb13
LiviaBacellar <32078993+LiviaBacellar@users.noreply.github.com>
2017-09-18 19:10:54 -0300

    calcula o fatorial

---
+++ 18.09.c
@@ -0,0 +1,30 @@
+/**************
+*Nome: Lívia Brito
+* Professor: Alex
+**********/
+
+#include <stdio.h>
+
+int main ()
+{
+
+    int num;
+    int i;
+    int fat;
+
+    do{
+        printf("nEntre com um numero:");
+        scanf ("%d", &num);
+    }
+    while (num<=0);
+    //calcular o fatorial;
+    fat=1;
+    for(i=num; i>1; i--)
+    {
+        fat = fat * i;
+    }
+    printf("\n0 fatorial = %d\n", fat);
+
+    system ("pause");
+    return 0;
+}
